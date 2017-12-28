6bc97fd933adfe9398ccb65cf0cf830a28666585
Edward Choi <edwardchoi.2011@gmail.com>
2017-12-28 13:17:53 -0800

    Retrieve_Users

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,19 @@
  require 'httparty'
+ require 'json'
  
  class Kele
-   #include httparty
+   include HTTParty
  
    def initialize(email, password)
      response = self.class.post("https://www.bloc.io/api/v1/sessions", 
      	body: { "email": email, "password": password })
      @auth_token = response["auth_token"]
      raise "Invalid email/password combination." if @auth_token.nil?
+   end
+
+   def get_me
+   	 url = "https://www.bloc.io/api/v1/users/me"
+     response = self.class.get(url, headers: { "authorization" => @auth_token })
+     @user_info = JSON.parse(response.body)
    end
  end
